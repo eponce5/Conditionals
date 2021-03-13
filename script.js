100b132ffb016bb9c5cc1905d0e800fd45d105c5
eponce5 <eponce5@mail.valenciacollege.edu>
2021-03-13 10:22:16 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
 
+var coinFlip = Math.round(Math.random()) + 1;
 
     var userChoice = prompt("heads or tails?");
 
-    var computerChoice = Math.random();
-    Math.floor(Math.random() * 2);
-    if (Math.random() < 0.5) {
-        println("heads!");
-      } else {
-        println("tails!");
-      }
-    console.log(y)
-    console.log(computerChoice);
-    var heads = computerChoice <= 0;
-    var tails = computerChoice >= 1;
+    if (coinFlip == 1) {
+        var flipResult = "heads";
+    } else {
+        var flipResult = "tails";
+    }
 
-    var val = randomInt(0, 1);
-if (val === 0 ) {
-  println("Heads");
-  var val = randomInt(0, 1);
-if (val === 2 ) {
-  println("Tails");
-   
-}
-var userChoice = prompt("What year were you born?");
-    const birthYear = 2001; n 
-} if (( 2021 - birthYear) < 21 ) {
- alert ("Sorry, you are not old enough to drink in the US");
+    if (flipResult == userChoice) {
+        if (flipResult == "heads") {
+            alert("You guessed right! The coin flip landed on " + flipResult + ".");
+        } else {
+            alert("You guessed right! The coin flip landed on " + flipResult + ".");
+        }
+    } else {
+        if (flipResult == "heads") {
+            alert("Sorry, the coin flip landed on " + flipresult + ".");
+        } else {
+            alert("Sorry, the coin flip landed on " + flipresult + ".");
+        }
+    }
+
+
+ birthYear = prompt("What year were you born?");
+
+const birthYear = 1999; n 
+ if (( 2021 - birthYear) < 22) {
+alert("You are old enough to drink in the US");
+   } else if (( 2021 - birthyear) < 21) {
+    alert ("You are old enough to drink in the US...barely");
+       } else { (( 2021- birthYear < 20)) 
+         alert ("Sorry, you are not old enough to drink in the US");
+
+
+console.log(birthYear)
 
 }
